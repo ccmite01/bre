9abfe2f1ace6e06a67ee6502db52b182f6ddf8ce
ccmite01 <ccmite01@gmail.com>
2021-12-31 06:40:21 +0000

    add20211231_1

--- start.sh
+++ start.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 if [ ! -d /opt/minecraft/${MC_INSTANCE_NAME} ]
-	            then
-			                            mkdir -p /opt/minecraft/${MC_INSTANCE_NAME}
-						                                chown www-data:www-data /opt/minecraft/${MC_INSTANCE_NAME}
+            then
+                        mkdir -p /opt/minecraft/${MC_INSTANCE_NAME}
+                            chown www-data:www-data /opt/minecraft/${MC_INSTANCE_NAME}
 fi
 
 if [ ! -d /var/www/html/console ]
-	            then
-			                            tar -x -v -f /console.tar.gz -C /var/www/html/
-						                                chmod +x /var/www/html/console/*.sh
-										                                sed -i "s/ccmite/${MC_INSTANCE_NAME}/g" /var/www/html/console/config/config.php
+            then
+                        tar -x -v -f /console.tar.gz -C /var/www/html/
+                            chmod +x /var/www/html/console/*.sh
+                                sed -i "s/ccmite/${MC_INSTANCE_NAME}/g" /var/www/html/console/config/config.php
 fi
 
 cd /opt/minecraft/${MC_INSTANCE_NAME}
-if [ ! -e papermc-${MCBE_VERSION}-${MCBE_BUILD}.jar ]
-	          then
-			                       curl -s -k --tlsv1.2 https://minecraft.azureedge.net/bin-linux/bedrock-server-${MCBE_VERSION}.${MCBE_BUILD}.zip -o bedrock-server.zip
-					                                  chown -R www-data:www-data *
+if [ ! -e bedrock_server ]
+          then
+                     curl -s -k --tlsv1.2 https://minecraft.azureedge.net/bin-linux/bedrock-server-${MCBE_VERSION}.${MCBE_BUILD}.zip -o bedrock-server.zip
+                           unzip -q bedrock-server.zip
+                           chown -R www-data:www-data *
 fi
 screen -S apc /usr/sbin/apachectl start
 mkdir -p /opt/minecraft/ssh
@@ -25,4 +26,3 @@ chown root:root /opt/minecraft/ssh/authorized_keys
 chmod 600 /opt/minecraft/ssh/authorized_keys
 /etc/init.d/ssh start
 screen -S mcs su -s /bin/bash - www-data -c "export TZ=JST-9; export LANG=ja_JP.UTF-8; cd /opt/minecraft/${MC_INSTANCE_NAME}; LD_LIBRARY_PATH=. ./bedrock_server"
-
